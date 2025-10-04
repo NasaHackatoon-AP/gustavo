fd86043afbc2555f9f083105afcfb390354d30f1
cleniosilva <clenioeduardo1994@gmail.com>
2025-10-04 17:37:50 -0300

    fix: ajustado integraccoes com apis externas

--- airmonitor/monitor.py
+++ airmonitor/monitor.py
@@ -4,23 +4,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-NASA_TEMPO_API = os.getenv("NASA_TEMPO_API")
+OPENAQ_API = os.getenv("OPENAQ_API")
 NASA_API_KEY = os.getenv("NASA_API_KEY")
 
-def obter_aqi_nasa_tempo_geo(lat: float, lon: float):
+def obter_aqi_nasa_tempo_geo(lat: float, lon: float, raio_em_metros: int = 5000):
     """
     Consulta AQI da NASA TEMPO usando latitude e longitude.
     """
     try:
-        params = {
-            "lat": lat,
-            "lon": lon,
-            "api_key": NASA_API_KEY
+               
+        headers = {
+        "accept": "application/json",
+        "X-API-Key": NASA_API_KEY
         }
-        resp = requests.get(NASA_TEMPO_API, params=params, timeout=10)
+        params = {
+            "coordinates": f"{lat},{lon}",
+            "radius": raio_em_metros,
+            "limit": 100,
+            "order_by": "distance"
+            }
+        print(f"Buscando estações de qualidade do ar perto de ({lat}, {lon})...")
+        resp = requests.get(f"{OPENAQ_API}/locations", headers=headers, params=params, timeout=15)
         resp.raise_for_status()
         data = resp.json()
-        return int(data.get("aqi", 50))  # fallback padrão
+        return data
     except Exception as e:
         print(f"Erro ao obter AQI da NASA: {e}")
-        return 50
+     
